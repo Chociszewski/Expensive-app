90c9ec72cd5ddd0adeab788cef69b3af0dd9129e
chocisz-iMac <chocisz@PC.local>
2018-11-26 21:28:17 +0100

    redux

---
+++ src/playground/destructuring.jsx
@@ -0,0 +1,50 @@
+//Objec destructuring
+
+// const person = {
+//   name: "andrew",
+//   age: 26,
+//   location: {
+//     city: "Philadelphia",
+//     temp: 92
+//   }
+// };
+
+// const { name: firstName = "anonymous", age } = person;
+// console.log(`${firstName} is ${age}`);
+
+// const { city, temp: temperature } = person.location;
+// if (city && temperature) {
+//   console.log(`It's ${temperature} in ${city}`);
+// }
+
+// const book = {
+//   title: "Ego is the Enemy",
+//   author: "Ryan Holigay",
+//   publisher: {
+//     // name: "Penguin"
+//   }
+// };
+
+// const { name: publisherName = "Self - Published" } = book.publisher;
+
+// console.log(publisherName);
+
+//
+//Array destructuring
+//
+
+const address = [
+  "1299 S Juniper Street",
+  "Philadelphia",
+  "Pensylvania",
+  "19147"
+];
+
+const [, city, state = "New York"] = address;
+
+console.log(`You are in ${city} ${state}`);
+
+const Item = ["coffe (hot)", "$2.00", "$2.50", "2.75"];
+
+const [coffe, , cost] = Item;
+console.log(`A medium ${coffe} costs ${cost}`);
---
+++ src/playground/redux-101.jsx
@@ -0,0 +1,40 @@
+import { createStore } from "redux";
+
+// Action generators - function that return action objects
+
+const incrementCount = () => ({
+  type: "INCREMENT"
+});
+
+const store = createStore((state = { count: 0 }, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      const incrementBy =
+        typeof action.incrementBy === "number" ? action.incrementBy : 1;
+      return {
+        count: state.count + incrementBy
+      };
+    case "DECREMENT":
+      const decrementBy =
+        typeof action.decrementBy === "number" ? action.decrementBy : 1;
+      return {
+        count: state.count - decrementBy
+      };
+    case "RESET":
+      return {
+        count: (state.count = 0)
+      };
+    default:
+      return state;
+  }
+});
+
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch({ type: "INCREMENT", incrementBy: 5 });
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "DECREMENT" });
+store.dispatch({ type: "DECREMENT", decrementBy: 10 });
+store.dispatch({ type: "RESET" });
